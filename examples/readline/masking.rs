0f398cebcc3f3278ac81a075f9d2ca72cf9c7893
ynqa <un.pensiero.vano@gmail.com>
2023-09-07 19:42:15 +0900

    remove old implementations

--- examples/readline/masking.rs
+++
@@ -1,9 +0,0 @@
-use promkit::{build::Builder, readline, Result};
-
-fn main() -> Result<()> {
-    let mut p = readline::Builder::default().mask('\0').build()?;
-    loop {
-        let line = p.run()?;
-        println!("result: {:?}", line);
-    }
-}
--- examples/readline/num_lines.rs
+++
@@ -1,9 +0,0 @@
-use promkit::{build::Builder, readline, Result};
-
-fn main() -> Result<()> {
-    let mut p = readline::Builder::default().num_lines(1).build()?;
-    loop {
-        let line = p.run()?;
-        println!("result: {:?}", line);
-    }
-}
