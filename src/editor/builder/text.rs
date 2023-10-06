d397d1f00e9a1f300525013bf020b7dc486cf22b
ynqa <un.pensiero.vano@gmail.com>
2023-10-06 12:38:09 +0900

    builder; contentstyle

--- src/editor/builder/text.rs
+++ src/editor/builder/text.rs
@@ -1,16 +1,23 @@
 use anyhow::Result;
 
-use crate::{editor::text::Text, grapheme::Graphemes};
+use crate::{editor::text::Text, grapheme::Graphemes, crossterm::style::ContentStyle};
 
 pub struct TextBuilder {
     text: Graphemes,
+    style: ContentStyle,
 }
 
 impl TextBuilder {
     pub fn new<T: AsRef<str>>(text: T) -> Self {
         Self {
             text: Graphemes::from(text),
+            style: ContentStyle::new(),
         }
+    }
+
+    pub fn style(mut self, style: ContentStyle) -> Self {
+        self.style = style;
+        self
     }
 
     pub fn build(self) -> Result<Box<Text>> {
