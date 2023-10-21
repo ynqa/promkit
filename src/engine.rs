1023a52079aeea6d6c05d3484bd1723b9d68a39e
ynqa <un.pensiero.vano@gmail.com>
2023-10-21 10:11:16 +0900

    clippy

--- src/engine.rs
+++ src/engine.rs
@@ -21,27 +21,27 @@ impl<W: Write> Engine<W> {
     }
 
     pub fn position(&self) -> Result<(u16, u16)> {
-        cursor::position().map_err(|err| Error::from(err))
+        cursor::position().map_err(Error::from)
     }
 
     pub fn size(&self) -> Result<(u16, u16)> {
-        terminal::size().map_err(|err| Error::from(err))
+        terminal::size().map_err(Error::from)
     }
 
     pub fn clear_from_cursor_down(&mut self) -> Result {
-        execute!(self.out, Clear(ClearType::FromCursorDown)).map_err(|err| Error::from(err))
+        execute!(self.out, Clear(ClearType::FromCursorDown)).map_err(Error::from)
     }
 
     pub fn clear(&mut self) -> Result {
-        execute!(self.out, Clear(ClearType::All), MoveTo(0, 0)).map_err(|err| Error::from(err))
+        execute!(self.out, Clear(ClearType::All), MoveTo(0, 0)).map_err(Error::from)
     }
 
     pub fn write<D: fmt::Display>(&mut self, string: D) -> Result {
-        execute!(self.out, Print(string)).map_err(|err| Error::from(err))
+        execute!(self.out, Print(string)).map_err(Error::from)
     }
 
     pub fn move_to(&mut self, pos: (u16, u16)) -> Result {
-        execute!(self.out, MoveTo(pos.0, pos.1)).map_err(|err| Error::from(err))
+        execute!(self.out, MoveTo(pos.0, pos.1)).map_err(Error::from)
     }
 
     pub fn is_bottom(&self) -> Result<bool> {
@@ -49,11 +49,11 @@ impl<W: Write> Engine<W> {
     }
 
     pub fn scroll_up(&mut self, times: u16) -> Result {
-        execute!(self.out, ScrollUp(times)).map_err(|err| Error::from(err))
+        execute!(self.out, ScrollUp(times)).map_err(Error::from)
     }
 
     pub fn move_to_next_line(&mut self) -> Result {
-        execute!(self.out, cursor::MoveToNextLine(1)).map_err(|err| Error::from(err))
+        execute!(self.out, cursor::MoveToNextLine(1)).map_err(Error::from)
     }
 }
 
