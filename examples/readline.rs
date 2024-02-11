27b4bf0a1a5699ed51f49a2a91bc95b6851fca2a
ynqa <un.pensiero.vano@gmail.com>
2024-02-11 15:06:33 +0900

    readline: remove validator

--- examples/readline.rs
+++ examples/readline.rs
@@ -1,13 +1,7 @@
 use promkit::{error::Result, preset::Readline};
 
 fn main() -> Result {
-    let mut p = Readline::default()
-        .title("Feel free to fill in")
-        .validator(
-            |text| text.len() > 10,
-            |text| format!("Length must be over 10 but got {}", text.len()),
-        )
-        .prompt()?;
+    let mut p = Readline::default().title("Feel free to fill in").prompt()?;
     println!("result: {:?}", p.run()?);
     Ok(())
 }
