833661d38c7c1e7e40a99939d378fa53bd94ab48
ynqa <un.pensiero.vano@gmail.com>
2024-03-01 20:40:11 +0900

    rm: text/ dir and mv into text.rs

--- src/core/text.rs
+++ src/core/text.rs
@@ -1,2 +1,46 @@
-mod render;
-pub use render::Renderer;
+use std::any::Any;
+
+use crate::{
+    crossterm::{event::Event, style::ContentStyle},
+    grapheme::{matrixify, StyledGraphemes},
+    pane::Pane,
+    AsAny, EventAction, Result,
+};
+
+/// A renderer for displaying text within a pane.
+///
+/// This struct is responsible for rendering text with a specific style
+/// and handling events that are relevant to the rendered text.
+#[derive(Clone)]
+pub struct Renderer {
+    /// The text to be rendered.
+    pub text: String,
+
+    /// Style for the text string.
+    pub style: ContentStyle,
+}
+
+impl crate::Renderer for Renderer {
+    fn make_pane(&self, width: u16) -> Pane {
+        Pane::new(
+            matrixify(
+                width as usize,
+                &StyledGraphemes::from_str(&self.text, self.style),
+            ),
+            0,
+            None,
+        )
+    }
+
+    fn handle_event(&mut self, _event: &Event) -> Result<EventAction> {
+        Ok(EventAction::Continue)
+    }
+
+    fn postrun(&mut self) {}
+}
+
+impl AsAny for Renderer {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
