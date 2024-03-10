81f39b1117822577ca0cd14555cefa4769000976
ynqa <un.pensiero.vano@gmail.com>
2024-03-10 12:57:52 +0900

    NOW ON ERROR: change to new keymap.rs

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,40 +1,6 @@
 use std::collections::HashMap;
 
-use crate::{
-    crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers},
-    Error, EventAction, EventHandler, Result,
-};
-
-/// Defines a minimal keymap for handling basic events such as quitting the application
-/// or interrupting with ctrl+c. It matches specific key events and returns an appropriate
-/// `EventAction` or an `Error`.
-///
-/// # Arguments
-///
-/// * `_`: A mutable reference to a generic state `S`, not used in this function.
-/// * `event`: A reference to the `Event` to be handled.
-///
-/// # Returns
-///
-/// This function returns a `Result<EventAction>` indicating the action to be taken in response
-/// to the event, or an `Error` if the event indicates an interruption.
-fn minimum_keymap<S>(_: &mut S, event: &Event) -> Result<EventAction> {
-    match event {
-        Event::Key(KeyEvent {
-            code: KeyCode::Enter,
-            modifiers: KeyModifiers::NONE,
-            kind: KeyEventKind::Press,
-            state: KeyEventState::NONE,
-        }) => Ok(EventAction::Quit),
-        Event::Key(KeyEvent {
-            code: KeyCode::Char('c'),
-            modifiers: KeyModifiers::CONTROL,
-            kind: KeyEventKind::Press,
-            state: KeyEventState::NONE,
-        }) => Err(Error::Interrupted("ctrl+c".into())),
-        _ => Ok(EventAction::Continue),
-    }
-}
+use crate::EventHandler;
 
 /// `KeymapManager` manages a collection of key-event handlers, allowing for dynamic switching
 /// and retrieval of event handlers based on a string key. It supports registering new key-handler
@@ -54,26 +20,14 @@ fn minimum_keymap<S>(_: &mut S, event: &Event) -> Result<EventAction> {
 pub struct KeymapManager<S> {
     mapping: HashMap<String, EventHandler<S>>,
     active_key: String,
-    default: EventHandler<S>,
 }
 
 impl<S> KeymapManager<S> {
-    /// Creates a new `KeymapManager` with a specified initial key and handler.
-    ///
-    /// # Arguments
-    ///
-    /// * `key`: A string slice that references the initial key to be used.
-    /// * `handler`: An `EventHandler` associated with the initial key.
-    ///
-    /// # Returns
-    ///
-    /// Returns an instance of `KeymapManager`.
     pub fn new<K: AsRef<str>>(key: K, handler: EventHandler<S>) -> Self {
         let key = key.as_ref().to_string();
         Self {
             mapping: HashMap::new(),
             active_key: key.clone(),
-            default: minimum_keymap,
         }
         .register(key, handler)
     }
@@ -114,17 +68,8 @@ impl<S> KeymapManager<S> {
         &self.active_key
     }
 
-    /// Retrieves the current `EventHandler` based on the current key.
-    ///
-    /// # Returns
-    ///
-    /// Returns a reference to the current `EventHandler`. If the current key does not have an
-    /// associated handler, the default handler is returned.
-    pub fn get(&self) -> &EventHandler<S> {
-        match self.mapping.get(&self.active_key) {
-            Some(handler) => handler,
-            None => &self.default,
-        }
+    pub fn get(&self) -> Option<&EventHandler<S>> {
+        self.mapping.get(&self.active_key)
     }
 }
 
@@ -134,12 +79,13 @@ mod tests {
 
     mod register {
         use super::*;
+        use crate::PromptSignal;
 
         #[test]
         fn test() {
             let mut manager =
-                KeymapManager::<()>::new("default", |_: &mut (), _| Ok(EventAction::Continue))
-                    .register("key2", |_: &mut (), _| Ok(EventAction::Continue));
+                KeymapManager::<()>::new("default", |_: &mut (), _| Ok(PromptSignal::Continue))
+                    .register("key2", |_: &mut (), _| Ok(PromptSignal::Continue));
             manager.switch("key2");
             assert_eq!("key2", manager.active_key)
         }
