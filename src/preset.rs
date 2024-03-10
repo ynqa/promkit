e553b77721e5c0222641713f6db4b2db7d933057
ynqa <un.pensiero.vano@gmail.com>
2024-03-10 21:12:23 +0900

    docs: preset

--- src/preset.rs
+++ src/preset.rs
@@ -1,7 +1,18 @@
+/// Provides a checkbox interface for multiple options selection.
 pub mod checkbox;
+
+/// Offers functionality for reading input from the user.
 pub mod readline;
 pub use readline::{confirm, password};
+
+/// Enables parsing and interaction with JSON data.
 pub mod json;
+
+/// Implements a list box for single or multiple selections from a list.
 pub mod listbox;
+
+/// Facilitates querying and selecting from a set of options in a structured format.
 pub mod query_selector;
+
+/// Supports creating and interacting with a tree structure for hierarchical data.
 pub mod tree;
