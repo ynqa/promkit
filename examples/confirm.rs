597251e4cf195c5fcf4f04eb2a602faf40a60c9f
ynqa <un.pensiero.vano@gmail.com>
2024-03-13 22:27:35 +0900

    chg: title for confirm

--- examples/confirm.rs
+++ examples/confirm.rs
@@ -1,7 +1,7 @@
 use promkit::{preset::confirm::Confirm, Result};
 
 fn main() -> Result {
-    let mut p = Confirm::new("Do you like programming?").prompt()?;
+    let mut p = Confirm::new("Do you have a pet?").prompt()?;
     println!("result: {:?}", p.run()?);
     Ok(())
 }
