a22f69d4094380f46690b312399c0c850b5cd3de
ynqa <un.pensiero.vano@gmail.com>
2024-05-12 18:57:00 +0900

    revert: from_iter for Listbox

--- promkit/src/core/listbox.rs
+++ promkit/src/core/listbox.rs
@@ -27,9 +27,7 @@ impl<T: fmt::Display> FromIterator<T> for Listbox {
     /// and collected into a `Vec<String>`.
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         Self(Cursor::new(
-            iter.into_iter()
-                .map(|e| format!("{}", e).replace('\n', " ").replace('\t', " "))
-                .collect(),
+            iter.into_iter().map(|e| format!("{}", e)).collect(),
             0,
             false,
         ))
