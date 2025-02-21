dd81874c01708e61b5ad0aa43225763be15b7a07
ynqa <un.pensiero.vano@gmail.com>
2025-02-21 22:28:59 +0900

    chore: bump up version to v0.8.0

--- promkit/src/lib.rs
+++ promkit/src/lib.rs
@@ -11,7 +11,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! promkit = "0.7.0"
+//! promkit = "0.8.0"
 //! ```
 //!
 //! ## Features
@@ -19,13 +19,13 @@
 //! - Support cross-platform both UNIX and Windows owing to [crossterm](https://github.com/crossterm-rs/crossterm)
 //! - Various building methods
 //!   - Preset; Support for quickly setting up a UI by providing simple parameters.
-//!     - [Readline](https://github.com/ynqa/promkit/tree/v0.7.0#readline)
-//!     - [Confirm](https://github.com/ynqa/promkit/tree/v0.7.0#confirm)
-//!     - [Password](https://github.com/ynqa/promkit/tree/v0.7.0#password)
-//!     - [Select](https://github.com/ynqa/promkit/tree/v0.7.0#select)
-//!     - [QuerySelect](https://github.com/ynqa/promkit/tree/v0.7.0#queryselect)
-//!     - [Checkbox](https://github.com/ynqa/promkit/tree/v0.7.0#checkbox)
-//!     - [Tree](https://github.com/ynqa/promkit/tree/v0.7.0#tree)
+//!     - [Readline](https://github.com/ynqa/promkit/tree/v0.8.0#readline)
+//!     - [Confirm](https://github.com/ynqa/promkit/tree/v0.8.0#confirm)
+//!     - [Password](https://github.com/ynqa/promkit/tree/v0.8.0#password)
+//!     - [Select](https://github.com/ynqa/promkit/tree/v0.8.0#select)
+//!     - [QuerySelect](https://github.com/ynqa/promkit/tree/v0.8.0#queryselect)
+//!     - [Checkbox](https://github.com/ynqa/promkit/tree/v0.8.0#checkbox)
+//!     - [Tree](https://github.com/ynqa/promkit/tree/v0.8.0#tree)
 //!   - Combining various UI components.
 //!     - They are provided with the same interface, allowing users to choose and
 //!       assemble them according to their preferences.
@@ -39,7 +39,7 @@
 //!
 //! ## Examples/Demos
 //!
-//! See [here](https://github.com/ynqa/promkit/tree/v0.7.0#examplesdemos)
+//! See [here](https://github.com/ynqa/promkit/tree/v0.8.0#examplesdemos)
 //!
 //! ## Why *promkit*?
 //!
