e6c0e31a71d384508126984a949d6e80b70dcda7
ynqa <un.pensiero.vano@gmail.com>
2025-07-11 05:26:12 +0900

    chore: remove OrderedIndex

--- promkit-core/src/render.rs
+++ promkit-core/src/render.rs
@@ -5,38 +5,6 @@ use tokio::sync::Mutex;
 
 use crate::{Pane, terminal::Terminal};
 
-/// OrderedIndex is used to represent the index of pane in a fraction format.
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub struct OrderedIndex(pub usize, pub usize); // numerator, denominator
-
-impl std::fmt::Display for OrderedIndex {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{})", self.0, self.1)
-    }
-}
-
-impl PartialOrd for OrderedIndex {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for OrderedIndex {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Comparing fractions: To compare a/b and c/d, compare ad and bc
-        let left = (self.0 as u64) * (other.1 as u64);
-        let right = (self.1 as u64) * (other.0 as u64);
-        left.cmp(&right)
-    }
-}
-
-impl OrderedIndex {
-    pub fn mediant(a: &OrderedIndex, b: &OrderedIndex) -> Self {
-        // TODO: gcd to reduce the fraction
-        Self(a.0 + b.0, a.1 + b.1)
-    }
-}
-
 /// SharedRenderer is a type alias for an Arc-wrapped Renderer, allowing for shared ownership and concurrency.
 pub type SharedRenderer<K> = Arc<Renderer<K>>;
 
