f903c0a719ee5025afb73fa2f6863004dcb4301a
ynqa <un.pensiero.vano@gmail.com>
2025-07-11 23:11:12 +0900

    chore: remove Content

--- promkit-core/src/render.rs
+++ promkit-core/src/render.rs
@@ -8,16 +8,10 @@ use crate::{Pane, terminal::Terminal};
 /// SharedRenderer is a type alias for an Arc-wrapped Renderer, allowing for shared ownership and concurrency.
 pub type SharedRenderer<K> = Arc<Renderer<K>>;
 
-/// Content represents the content of a pane, including its visibility status.
-pub struct Content {
-    pane: Pane,
-    visible: bool,
-}
-
 /// Renderer is responsible for managing and rendering multiple panes in a terminal.
 pub struct Renderer<K: Ord + Send + 'static> {
     terminal: Mutex<Terminal>,
-    contents: SkipMap<K, Content>,
+    panes: SkipMap<K, Pane>,
 }
 
 impl<K: Ord + Send + 'static> Renderer<K> {
@@ -26,16 +20,16 @@ impl<K: Ord + Send + 'static> Renderer<K> {
             terminal: Mutex::new(Terminal {
                 position: crossterm::cursor::position()?,
             }),
-            contents: SkipMap::new(),
+            panes: SkipMap::new(),
         })
     }
 
     pub fn update<I>(&mut self, items: I) -> &mut Self
     where
-        I: IntoIterator<Item = (K, Content)>,
+        I: IntoIterator<Item = (K, Pane)>,
     {
-        items.into_iter().for_each(|(index, content)| {
-            self.contents.insert(index, content);
+        items.into_iter().for_each(|(index, pane)| {
+            self.panes.insert(index, pane);
         });
         self
     }
@@ -45,17 +39,16 @@ impl<K: Ord + Send + 'static> Renderer<K> {
         I: IntoIterator<Item = K>,
     {
         items.into_iter().for_each(|index| {
-            self.contents.remove(&index);
+            self.panes.remove(&index);
         });
         self
     }
 
     pub async fn render(&mut self) -> anyhow::Result<()> {
         let panes: Vec<Pane> = self
-            .contents
+            .panes
             .iter()
-            .filter(|entry| entry.value().visible)
-            .map(|entry| entry.value().pane.clone())
+            .map(|entry| entry.value().clone())
             .collect();
         let mut terminal = self.terminal.lock().await;
         terminal.draw(&panes)
