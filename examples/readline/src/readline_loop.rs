83f5af0fbfc866b13c7b49abd34f6b1bf40daf06
ynqa <un.pensiero.vano@gmail.com>
2025-07-13 13:00:40 +0900

    chore: revert readline-loop

--- examples/readline/src/readline_loop.rs
+++ examples/readline/src/readline_loop.rs
@@ -2,18 +2,16 @@ use promkit::preset::readline::Readline;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // loop {
-    //     match Readline::default().prompt().await {
-    //         Ok(cmd) => {
-    //             println!("result: {:?}", cmd);
-    //         }
-    //         Err(e) => {
-    //             println!("Error: {}", e);
-    //             break;
-    //         }
-    //     }
-    // }
-    Readline::default().prompt().await?;
-    Readline::default().prompt().await?;
+    loop {
+        match Readline::default().prompt().await {
+            Ok(cmd) => {
+                println!("result: {:?}", cmd);
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
+        }
+    }
     Ok(())
 }
