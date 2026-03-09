49d128aeb69b025f86ab5327de50e167c61732b5
ynqa <un.pensiero.vano@gmail.com>
2026-03-10 03:45:33 +0900

    chore: create Session::spawn to start pseudo-terminal

--- termharness/src/session.rs
+++ termharness/src/session.rs
@@ -1,11 +1,15 @@
 use std::{
+    io::Read,
     io::Write,
     sync::{Arc, Mutex},
+    thread,
     thread::JoinHandle,
 };
 
+use anyhow::Result;
+use portable_pty::{Child, CommandBuilder, MasterPty, PtySize, native_pty_system};
+
 use crate::terminal::TerminalSize;
-use portable_pty::{Child, MasterPty};
 
 pub struct Session {
     pub child: Box<dyn Child + Send + Sync>,
@@ -14,4 +18,53 @@ pub struct Session {
     pub output: Arc<Mutex<Vec<u8>>>,
     pub reader_thread: Option<JoinHandle<()>>,
     pub size: TerminalSize,
+}
+
+impl Session {
+    /// Spawn a new session by executing the given command in a pseudo-terminal with the specified size.
+    pub fn spawn(mut cmd: CommandBuilder, size: TerminalSize) -> Result<Self> {
+        let pty = native_pty_system();
+        let pair = pty.openpty(PtySize {
+            rows: size.rows,
+            cols: size.cols,
+            pixel_width: 0,
+            pixel_height: 0,
+        })?;
+
+        // Set the TERM environment variable to ensure consistent terminal behavior.
+        // Considaration: This should ideally be configurable,
+        // but for now we hardcode it to ensure tests run reliably.
+        cmd.env("TERM", "xterm-256color");
+        let child = pair.slave.spawn_command(cmd)?;
+        drop(pair.slave);
+
+        let master = pair.master;
+        let output = Arc::new(Mutex::new(Vec::new()));
+        let output_reader = Arc::clone(&output);
+        let mut reader = master.try_clone_reader()?;
+        let reader_thread = thread::spawn(move || {
+            let mut buf = [0_u8; 4096];
+            loop {
+                match reader.read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(n) => output_reader
+                        .lock()
+                        .expect("failed to lock output buffer")
+                        .extend_from_slice(&buf[..n]),
+                    Err(err) if err.kind() == std::io::ErrorKind::Interrupted => continue,
+                    Err(_) => break,
+                }
+            }
+        });
+
+        let writer = master.take_writer()?;
+        Ok(Self {
+            child,
+            master,
+            writer,
+            output,
+            reader_thread: Some(reader_thread),
+            size,
+        })
+    }
 }
